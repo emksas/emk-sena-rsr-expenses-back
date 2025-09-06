bd6888b1f615049e8f7c891cf4bd7f8ad83442ee
ラムセスソラノラミレズ <ramsessr@icloud.com>
2025-09-06 12:06:06 -0500

    HIDE SECRETS

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,10 +1,12 @@
 const msal = require('@azure/msal-node');
+const { OUTLOOK_CLIENT_ID, OUTLOOK_AUTHORITY, OUTLOOK_CLIENT_SECRET } = process.env;
+
 
 const cca = new msal.ConfidentialClientApplication({
     auth: {
-        clientId: '85bb9e6b-a3d2-45ab-9014-c803bd3d2e5b',
-        authority: `https://login.microsoftonline.com/16c21427-f2c0-4501-ba6e-2c065e2ca7f7`,
-        clientSecret: 'Qxr8Q~qEsb~RJ4Bbf8YvOpcqdWwT3cx7Kp4r3c7l',
+        clientId: OUTLOOK_CLIENT_ID,
+        authority: OUTLOOK_AUTHORITY,
+        clientSecret: OUTLOOK_CLIENT_SECRET,
     },
 });
 
