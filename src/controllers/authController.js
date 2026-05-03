1f3efe575704522cf31494f11d18880a266c2ce2
Ramses Solano Ramirez <ramsessr@outlook.com>
2026-05-03 05:36:08 -0500

    controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,13 @@
 import { buildAuthUrl, handleAuthCode } from "../services/msAuthService.js";
-import  { getUserByIdAndHomeAccountId, createUserInformation }  from "../services/UserService.js";
+import {
+  getUserByIdAndHomeAccountId,
+  createUserInformation,
+  getUserById
+} from "../services/UserService.js";
 
 async function authLogin(req, res, next) {
   try {
-    let url = await buildAuthUrl( req.query.id );
+    let url = await buildAuthUrl(req.query.id);
     res.redirect(url);
   } catch (e) {
     next(e);
@@ -12,18 +16,24 @@ async function authLogin(req, res, next) {
 
 async function authRedirect(req, res, next) {
   try {
-
     const { code, state } = req.query;
     const parsedState = state ? JSON.parse(state) : {};
     const userId = parsedState?.userId;
     const result = await handleAuthCode(code);
-    const existingUser = await getUserByIdAndHomeAccountId(userId, result.tokenByCode.account.homeAccountId);
+
+    const existingUser = await getUserById(userId);
+    //const existingUser = await getUserByIdAndHomeAccountId(userId, result.tokenByCode.account.homeAccountId);
+
+    console.log("Usuario existente en la base de datos:");
+    console.log(existingUser);
 
     if (existingUser) {
       return res.send(
-        `✅ Usuario ${existingUser.username} ya está autenticado.`
+        `✅ Usuario ${existingUser[0].username} ya está autenticado.`,
       );
     } else {
+      console.log("Creando nuevo usuario con la información obtenida...");
+
       const user = {
         user_id: userId,
         home_account_id: result.tokenByCode.account.homeAccountId,
@@ -35,7 +45,6 @@ async function authRedirect(req, res, next) {
     }
 
     res.send(`✅ Autenticado como ${result.tokenByCode.account.username}.`);
-
   } catch (e) {
     next(e);
   }
